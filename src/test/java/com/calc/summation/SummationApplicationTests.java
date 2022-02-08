ff8dfbace26b1702645ae603ac96cae9b20ff4fc
Your Name <esraa.hamouda@dentacarts.com>
2022-02-08 17:16:14 +0200

    init commit

---
+++ src/test/java/com/calc/summation/SummationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calc.summation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
