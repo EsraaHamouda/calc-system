3c5a04b7450e93a30f78edd568fb81acfdfbeba6
Your Name <esraa.hamouda@dentacarts.com>
2022-02-08 20:22:10 +0200

    add list summation

--- src/main/java/com/calc/summation/controller/SummationController.java
+++ src/main/java/com/calc/summation/controller/SummationController.java
@@ -1,9 +1,8 @@
 package com.calc.summation.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/sum")
@@ -13,4 +12,9 @@ public class SummationController {
     public int sum(@PathVariable("param1") Integer param1, @PathVariable("param2") Integer param2) {
         return param1 + param2;
     }
+    @PostMapping("/list")
+    public int sum(@RequestBody ArrayList<Integer> list) {
+        return list.stream().reduce(0, Integer::sum);
+    }
+
 }
